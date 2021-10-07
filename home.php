3e806272452f45d94aaed9851a33b5628d92b5d3
Adrian Garcia Silva <adrian.gs.016@gmail.com>
2021-10-07 12:25:49 -0600

    mejora en estilos 1
    se cambio a mejor el estilo del login

--- home.php
+++ home.php
@@ -16,12 +16,15 @@
 <body class="bg-dark">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
+          <a class="navbar-brand" href="">Navbar</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="home-de-prueba.html">nav de prueba</a>
+              </li>
               <li class="nav-item">
                 <a class="nav-link" href="Login/log_out.php">Log-out</a>
               </li>
--- index.php
+++ index.php
@@ -10,39 +10,69 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
 
-<!--  -->    <!-- Scripts externos -->    <!--  -->
                     <!-- Bootstrap5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/Bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="css/Bootstrap/bootstrap.css.map">
+    <link rel="stylesheet" href="css/Bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="css/Bootstrap/bootstrap.min.css.map">
 
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/MyStyles.css">
 </head>
-<body class="bg-dark">
-    <!-- Diseño del login creado usando bootstrap -->
-    <div class="d-flex align-content-center p-4 mt-2">
-        <div class="container bg-light" id="D_Container rounded">
-            <div class="text-center p-2">
-                <h3>Login</h3>
+<body class="bg-secondary">
+<div>
+<div class="middle-box text-center loginscreen animated fadeInDown gray-bg rounded shadow-lg MrgSty">
+        <div class="p-3">
+            <div class="text-center">
+                <!-- <h2 class="display-5">Logo</h2> -->
+                <img src="img/Logo.png" alt="Imagen del logo">
             </div>
-            <form action="index.php" method="POST">
-                <div class="mb-3">
-                    <!-- input del username -->
-                  <input id="LoginUsername" name="LoginUsername" type="text" class="form-control" placeholder="Nombre de Usuario" autocomplete="off" required>
+            <div class="my-3">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aliquam molestias unde doloremque minima optio?
+            </div>
+            <!-- Formulario del login -->
+            <div>
+                <div class="text-center p-2">
+                    <h4>Iniciar Sesion</h4>
                 </div>
-                <div class="mb-3">
-                    <!-- input del password -->
-                  <input id="LoginPassword" name="LoginPassword" type="password" class="form-control" placeholder="Contraseña" autocomplete="off" required>
-                </div>
-                <div class="pb-4 d-grid g-0">
-                    <button type="submit" class="btn btn-outline-primary">Submit</button>
-                </div>
-              </form>
-        </div>
+                <form action="index.php" method="POST" class="text-muted">
+                    <div class="mb-3">
+                        <!-- input del username -->
+                        <input id="LoginUsername" name="LoginUsername" type="text" class="form-control" placeholder="Nombre de Usuario" autocomplete="off" required>
+                    </div>
+                    <div class="mb-3">
+                        <!-- input del password -->
+                        <input id="LoginPassword" name="LoginPassword" type="password" class="form-control" placeholder="Contraseña" autocomplete="off" required>
+                    </div>
+                    <div class="pb-1 d-grid g-0">
+                        <button type="submit" class="btn bg-success">Ingresar</button>
+                    </div>
+                </form>
+                <!-- formulario el cual añade un boton para ir a registrar un nuevo usuario --> <!-- se encuentra escondido al no se necesario -->
+                <form action="CreateAcount.php" method="POST" class="d-none">
+                    <hr>
+                    <div class="mb-3">
+                        <p class="text-white"> no tienes cuenta? </p>
+                    </div>
+                    <div class="mb-3">
+                        <button type="submit" class="btn bg-info"> crea una cuenta</button>
+                    </div>
+                </form>
+            </div>
+        </div>  
     </div>
-    <?php
-        echo $ErrorLogin;
-    ?>
-
+    <div class="animated fadeInDown">
+        <?php
+            echo $ErrorLogin;
+        ?>
+    </div>
+</div>
 <!--  -->    <!-- Scripts externos -->    <!--  -->
                     <!-- Bootstrap5 -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="javaScripts/Bootstrap/bootstrap.js"></script>
+    <script src="javaScripts/Bootstrap/bootstrap.js.map"></script>
+    <script src="javaScripts/Bootstrap/bootstrap.min.js"></script>
+    <script src="javaScripts/Bootstrap/bootstrap.min.js.map"></script>
 </body>
 </html>
