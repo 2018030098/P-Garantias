342231d4d36fe047ee69ec3a0a55f91e0e7f63a0
Adrian Garcia Silva <adrian.gs.016@gmail.com>
2021-10-12 16:20:12 -0600

    submenu en el navbar lateral

--- Login/Info.php
+++ Login/Info.php
@@ -1,12 +1,14 @@
 <?php
 // Obtiene la informacion registrada en la base de datos del usuario que acaba de ingresar
 try {
-        $stmt -> bind_result($id, $usr, $psw, $name, $img);
+        $stmt -> bind_result($id, $usr, $psw, $email, $name, $gender, $img);
         while ($stmt->fetch()) {
                 $_SESSION['id'] = $id;
                 $_SESSION['Usr'] = $usr;
                 $_SESSION['psw'] = $psw;
+                $_SESSION['email'] = $email;
                 $_SESSION['Name'] = $name;
+                $_SESSION['gender'] = $gender;
                 $_SESSION['img'] = $img;
         }
         // por seguridad se les quita el valor a estos campos, ademas de que no son necesarios
